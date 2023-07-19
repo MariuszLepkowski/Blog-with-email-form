a98e03ab71ebcbe6b3ec12b6a5b2d38c14915cf0
Mariusz Lepkowski <mariusz.lepkowski23@gmail.com>
2023-07-19 14:37:28 +0200

    Change debug mode to False.

--- main.py
+++ main.py
@@ -19,6 +19,29 @@ posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 app = Flask(__name__)
 
 
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    context = ssl.create_default_context()
+    with smtplib.SMTP(SMTP_SERVER, port=587, timeout=120) as connection:
+        logging.debug(f"Server Address: {SMTP_SERVER}")
+        logging.debug(f"Server Port: {587}")
+        try:
+            connection.starttls(context=context)
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            connection.sendmail(
+                        from_addr=MY_EMAIL,
+                        to_addrs=RECEIVER_EMAIL,
+                        msg=email_message
+            )
+            logging.info("Email sent successfully!")
+        except ConnectionRefusedError as e:
+            logging.error(f"Connection to SMTP server failed: {e}")
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
+        finally:
+            connection.close()
+
+
 @app.route('/')
 def get_all_posts():
     return render_template("index.html", all_posts=posts)
@@ -53,28 +76,5 @@ def show_post(index):
     return render_template("post.html", post=requested_post)
 
 
-def send_email(name, email, phone, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
-    context = ssl.create_default_context()
-    with smtplib.SMTP(SMTP_SERVER, port=587, timeout=120) as connection:
-        logging.debug(f"Server Address: {SMTP_SERVER}")
-        logging.debug(f"Server Port: {587}")
-        try:
-            connection.starttls(context=context)
-            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-            connection.sendmail(
-                        from_addr=MY_EMAIL,
-                        to_addrs=RECEIVER_EMAIL,
-                        msg=email_message
-            )
-            logging.info("Email sent successfully!")
-        except ConnectionRefusedError as e:
-            logging.error(f"Connection to SMTP server failed: {e}")
-        except Exception as e:
-            logging.error(f"An unexpected error occurred: {e}")
-        finally:
-            connection.close()
-
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
